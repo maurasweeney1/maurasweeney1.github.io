0360f157ad0c4563fedbab1a556af2392647663a
Maura Sweeney <123509194+maurasweeney1@users.noreply.github.com>
2024-12-22 22:05:17 -0800

    integration

---
+++ react-portfolio/src/components/Sidebar/index.js
@@ -0,0 +1,54 @@
+import { NavLink } from 'react-router-dom'
+import './index.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+const Sidebar = () => {
+  return (
+    <div className="nav-bar">
+      <nav>
+        <NavLink exact="true" activeclassName="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="about-link"
+          to="/about"
+        >
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/mauraksweeney"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/maurasweeney1"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+export default Sidebar
