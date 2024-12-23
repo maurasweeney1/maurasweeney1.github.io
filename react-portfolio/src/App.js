14363ea66b91cb42cdf37d4de97dd3467b41cca6
Maura Sweeney <123509194+maurasweeney1@users.noreply.github.com>
2024-12-23 09:35:42 -0800

    github pages

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,18 +1,18 @@
-import './App.scss';
+import './App.scss'
 import Home from './components/Home'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Layout from './components/Layout'
 
 function App() {
   return (
-    <>
-    <Routes>
-      <Route path = "/" element = {<Layout />}>
-        <Route index element = { <Home />} />
-      </Route>
-    </Routes>
-    </>
+    <BrowserRouter basename="/repository-name">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App;
+export default App
