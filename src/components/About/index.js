45b911880fb749e537ead412f4a6b971125e984f
Maura Sweeney <123509194+maurasweeney1@users.noreply.github.com>
2024-12-23 16:47:46 -0800

    fixed sit integratioin

---
+++ src/components/About/index.js
@@ -0,0 +1,14 @@
+import './index.scss'
+
+const About = () => {
+  return (
+    <div className="container about-page">
+      <div className="text-zone">
+        <h1>About Me</h1>
+        <p>// Your about content here</p>
+      </div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Contact/index.js
@@ -0,0 +1,14 @@
+import './index.scss'
+
+const Contact = () => {
+  return (
+    <div className="container contact-page">
+      <div className="text-zone">
+        <h1>Contact Me</h1>
+        // Your contact content here
+      </div>
+    </div>
+  )
+}
+
+export default Contact
